e9670a210cd804639f30da288cf9a83d71e22f03
Mateusz Krówczyński <krafcik226@gmail.com>
2024-08-29 19:06:04 +0200

    Initial Commit

---
+++ CryptoPocket/CryptoPocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPocketApp.swift
+//  CryptoPocket
+//
+//  Created by Mateusz Krówczyński on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
