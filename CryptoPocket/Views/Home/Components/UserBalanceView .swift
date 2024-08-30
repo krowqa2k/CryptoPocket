7f50ba23ec6634e19c182fd9cd813793aa476794
Mateusz Krówczyński <krafcik226@gmail.com>
2024-08-30 16:25:33 +0200

    feat
    added user balance view

--- CryptoPocket/Views/Home/Components/UserBalanceView .swift
+++ CryptoPocket/Views/Home/Components/UserBalanceView .swift
@@ -7,12 +7,42 @@
 
 import SwiftUI
 
-struct UserBalanceView_: View {
+struct UserBalanceView: View {
+    
+    @State private var isProfit: Bool = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 4) {
+            Text("Your Balance")
+                .font(.callout)
+                .fontWeight(.medium)
+                .foregroundStyle(.secondaryTextCP)
+            
+            HStack {
+                Text("$17258.13")
+                    .font(.system(size: 45))
+                    .fontWeight(.medium)
+                    .foregroundStyle(.textCP)
+                    .fontDesign(.rounded)
+                
+                VStack(alignment: .leading) {
+                    Text("+37,2%")
+                        .foregroundStyle(isProfit ? .green : .red)
+                    
+                    Text("All time")
+                        .font(.callout)
+                        .foregroundStyle(.secondaryTextCP.opacity(0.4))
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal)
     }
 }
 
 #Preview {
-    UserBalanceView_()
+    ZStack {
+        Color.backgroundCP.ignoresSafeArea()
+        UserBalanceView()
+    }
 }
