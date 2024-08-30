74c1618791df8079d8ee4195fb73ad293192eba0
Mateusz Krówczyński <krafcik226@gmail.com>
2024-08-30 16:24:54 +0200

    style
    changed background in home view

---
+++ CryptoPocket/Views/Home/Components/UserBalanceView .swift
@@ -0,0 +1,18 @@
+//
+//  UserBalanceView .swift
+//  CryptoPocket
+//
+//  Created by Mateusz Krówczyński on 30/08/2024.
+//
+
+import SwiftUI
+
+struct UserBalanceView_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserBalanceView_()
+}
--- CryptoPocket/Views/Home/HomeView .swift
+++ CryptoPocket/Views/Home/HomeView .swift
@@ -10,13 +10,22 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         ZStack {
-            Color.backgroundCP.ignoresSafeArea()
+            background
             
-            VStack {
+            VStack(spacing: 32) {
                 HeaderView()
+                
+                UserBalanceView()
                 
                 Spacer(minLength: 0)
             }
+        }
+    }
+    
+    private var background: some View {
+        ZStack {
+            Color.backgroundCP.ignoresSafeArea()
+            LinearGradient(colors: [Color.backgroundCP, Color.backgroundCP.opacity(0.8), Color.black.opacity(0.3), Color.black.opacity(0.4)], startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea()
         }
     }
 }
