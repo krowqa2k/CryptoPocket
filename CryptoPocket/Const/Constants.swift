326f4f772b0c76c0838763c89b52e1616d7ff19f
Mateusz Krówczyński <krafcik226@gmail.com>
2024-09-01 20:56:52 +0200

    feat
    added constants data with mock image and no image urls

---
+++ CryptoPocket/Const/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  CryptoPocket
+//
+//  Created by Mateusz Krówczyński on 01/09/2024.
+//
+
+import Foundation
+
+struct Constants {
+    static let mockImage: String = "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400"
+    static let noImage: String = "https://static-00.iconduck.com/assets.00/no-image-icon-2048x2048-2t5cx953.png"
+}
