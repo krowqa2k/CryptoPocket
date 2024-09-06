438fd6c9c82be05f1f97951159e5ab0cd87855f0
Mateusz Krówczyński <krafcik226@gmail.com>
2024-09-06 18:35:08 +0200

    feat
    created coin detail view which contains line graph with coin price change from the last 7 days and coin overview below.

---
+++ CryptoPocket/Views/CoinDetails/View/CoinDetailsView .swift
@@ -0,0 +1,74 @@
+//
+//  CoinDetailsView .swift
+//  CryptoPocket
+//
+//  Created by Mateusz Krówczyński on 06/09/2024.
+//
+
+import SwiftUI
+
+struct CoinDetailsView: View {
+    
+    @StateObject var viewModel: DetailViewModel
+    @State private var isFavorite: Bool = false
+    @Environment(\.dismiss) var dismiss
+    
+    init(coin: CoinModel) {
+        _viewModel = StateObject(wrappedValue: DetailViewModel(coin: coin))
+    }
+    
+    var body: some View {
+        ZStack {
+            background
+            
+            ScrollView(.vertical) {
+                VStack(spacing: 20) {
+                    LineChartView(coin: viewModel.coin)
+                        .padding(.horizontal)
+                    
+                    Divider()
+                        .overlay(Color.secondaryTextCP)
+                    
+                    CoinOverviewView(coin: viewModel.coin)
+                }
+                .padding(.top, 20)
+            }
+        }
+        .navigationTitle(viewModel.coin.name)
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                toolbarLeading
+            }
+        }
+    }
+    
+    private var background: some View {
+        NavigationStack {
+            ZStack {
+                Color.backgroundCP.ignoresSafeArea()
+                LinearGradient(colors: [Color.backgroundCP, Color.backgroundCP.opacity(0.8), Color.black.opacity(0.3), Color.black.opacity(0.4)], startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea()
+            }
+        }
+    }
+    
+    private var toolbarLeading: some View {
+        HStack(spacing: 4) {
+            Text(viewModel.coin.symbol.uppercased())
+            ImageLoader(imageURL: viewModel.coin.image)
+                .frame(width: 20, height: 20)
+            Button(action: {
+                isFavorite.toggle()
+            }, label: {
+                Image(systemName: "heart.fill")
+                    .foregroundStyle(isFavorite ? .red : .secondaryTextCP)
+            })
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        CoinDetailsView(coin: .mock)
+    }
+}
