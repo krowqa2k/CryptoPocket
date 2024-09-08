fbdffcfa1b474f46ef0951b03b5881af7d17a156
Mateusz Krówczyński <krafcik226@gmail.com>
2024-09-08 16:04:24 +0200

    style
    changed styling of user balance view

--- CryptoPocket/Views/Home/Views/UserBalanceView .swift
+++ CryptoPocket/Views/Home/Views/UserBalanceView .swift
@@ -12,31 +12,38 @@ struct UserBalanceView: View {
     @State private var isProfit: Bool = true
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 4) {
-            Text("Your Balance")
-                .font(.callout)
-                .fontWeight(.medium)
-                .foregroundStyle(.secondaryTextCP)
-            
+        VStack(alignment: .leading, spacing: 24) {
             HStack {
-                Text("$17258.13")
-                    .font(.system(size: 45))
+                Text("Your Balance")
+                    .font(.system(size: 18))
                     .fontWeight(.medium)
-                    .foregroundStyle(.textCP)
-                    .fontDesign(.rounded)
+                    .foregroundStyle(.secondaryTextCP)
                 
-                VStack(alignment: .leading) {
-                    Text("+37,2%")
-                        .foregroundStyle(isProfit ? .green : .red)
-                    
-                    Text("All time")
-                        .font(.callout)
-                        .foregroundStyle(.secondaryTextCP.opacity(0.4))
-                }
+                Text("+37,2%")
+                    .font(.callout)
+                    .foregroundStyle(.black)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 4)
+                    .background(
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(isProfit ? .green : .red)
+                    )
             }
+            
+            Text("$17258.13")
+                .font(.system(size: 45))
+                .fontWeight(.medium)
+                .foregroundStyle(.textCP)
+                .fontDesign(.rounded)
         }
         .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 12)
+                .foregroundStyle(.ultraThinMaterial.opacity(0.5))
+        )
         .padding(.horizontal)
+        
     }
 }
 
