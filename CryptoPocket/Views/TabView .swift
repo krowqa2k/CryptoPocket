c03a8dfc30dd4759eb36885f7eba1aa05a1ee0cb
Mateusz Krówczyński <krafcik226@gmail.com>
2024-09-10 21:51:03 +0200

    style
    changed tabview style to custom shape

--- CryptoPocket/Views/TabView .swift
+++ CryptoPocket/Views/TabView .swift
@@ -16,12 +16,9 @@ struct TabView: View {
     var body: some View {
         ZStack {
             ZStack {
-                UnevenRoundedRectangle(
-                    topLeadingRadius: 12,
-                    topTrailingRadius: 12
-                )
-                .fill(Gradient(colors: [Color.backgroundCP, Color.black.opacity(0.2)]))
-                .frame(width: width, height: 80)
+                TabViewRectangle()
+                    .fill(Gradient(colors: [Color.backgroundCP, Color.black.opacity(0.2)]))
+                    .frame(width: width, height: 80)
                 
                 HStack {
                     Button(action: {
@@ -63,7 +60,7 @@ struct TabView: View {
                         .foregroundStyle(.textCP)
                 })
             }
-            .offset(y: -30)
+            .offset(y: -10)
         }
     }
 }
@@ -73,5 +70,40 @@ struct TabView: View {
         Color.backgroundCP.ignoresSafeArea()
         LinearGradient(colors: [Color.backgroundCP, Color.backgroundCP.opacity(0.8), Color.black.opacity(0.3), Color.black.opacity(0.4)], startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea()
         TabView(index: .constant(0))
+    }
+}
+
+struct TabViewRectangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: 0, y: 0))
+                    path.addLine(to: CGPoint(x: rect.width * 0.35, y: 0))
+                    path.addArc(center: CGPoint(x: rect.width * 0.35, y: rect.height * 0.5),
+                                radius: rect.height * 0.5,
+                                startAngle: .degrees(180),
+                                endAngle: .degrees(90),
+                                clockwise: false)
+                    path.addLine(to: CGPoint(x: 0, y: rect.height))
+                    
+                    path.closeSubpath()
+
+                    let circleRadius = rect.height * 0.4
+                    path.addArc(center: CGPoint(x: rect.width * 0.5, y: rect.height - 30),
+                                radius: circleRadius,
+                                startAngle: .degrees(0),
+                                endAngle: .degrees(360),
+                                clockwise: false)
+
+                    path.move(to: CGPoint(x: rect.width * 0.65, y: 0))
+                    path.addLine(to: CGPoint(x: rect.width, y: 0))
+                    path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+                    path.addLine(to: CGPoint(x: rect.width * 0.65, y: rect.height))
+                    path.addArc(center: CGPoint(x: rect.width * 0.65, y: rect.height * 0.5),
+                                radius: rect.height * 0.5,
+                                startAngle: .degrees(90),
+                                endAngle: .degrees(-90),
+                                clockwise: false)
+                    path.closeSubpath()
+        }
     }
 }
