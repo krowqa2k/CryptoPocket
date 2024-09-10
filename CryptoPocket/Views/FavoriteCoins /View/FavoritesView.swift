8d46718e4fdff96a850c4a2f44725d1a260b41e7
Mateusz Krówczyński <krafcik226@gmail.com>
2024-09-10 21:53:30 +0200

    bug fix
    fixed bug where favorite coins view was on screen even when user didnt have any favorite coins added

--- CryptoPocket/Views/FavoriteCoins /View/FavoritesView.swift
+++ CryptoPocket/Views/FavoriteCoins /View/FavoritesView.swift
@@ -43,7 +43,7 @@ struct FavoritesView: View {
             }
             .scrollIndicators(.hidden)
         }
-        .opacity(viewModel.favoriteCoins.isEmpty ? 1 : 1.0)
+        .opacity(viewModel.favoriteCoins.isEmpty ? 0 : 1.0)
     }
 }
 
