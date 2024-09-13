476761bc9278b54888fa0e4db0477613688d0cb3
Mateusz Krówczyński <krafcik226@gmail.com>
2024-09-13 17:04:15 +0200

    feat
    added mock portfolio coin for creation purpose

--- CryptoPocket/Model/UserPortfolioModel .swift
+++ CryptoPocket/Model/UserPortfolioModel .swift
@@ -17,4 +17,17 @@ struct UserPortfolioModel: Identifiable {
     var currentHoldingsValue: Double {
             (currentHoldings ?? 0) * currentPrice
     }
+    
+    static var mockPortfolio: UserPortfolioModel {
+        UserPortfolioModel(
+            id: "bitcoin",
+            symbol: "btc",
+            name: "Bitcoin",
+            image: "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
+            currentPrice: 60898,
+            priceChange24H: 2155.47,
+            priceChangePercentage24H: 3.27,
+            currentHoldings: 0.11
+        )
+    }
 }
